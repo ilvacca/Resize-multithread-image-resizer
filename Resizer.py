fb70ac0559ebb3ee12f8b0e16617bd7684dade0f
Alessio Vaccaro <alexvac@hotmail.it>
2018-02-27 19:33:00 +0100

    Added comments

--- Resizer.py
+++ Resizer.py
@@ -9,21 +9,26 @@ root.withdraw()
 imagelist = []
 c=0
 
+# Input module
 filelist = tkFileDialog.askopenfilename(initialdir = "C:/",title = "Select photo folder",filetypes = (("JPEG files","*.jpg *.jpeg"),("PNG files","*.png"),("all files","*.*")),multiple=1)
 path = os.path.dirname(filelist[0])+"/"
 destination_folder=tkFileDialog.askdirectory(parent=root,initialdir=path,title='Destination directory')
+print "Found %d %s images in %s\n"%(len(imagelist),filelist[0].split(".")[-1],path)
 
+# Select destination extention
 destination_extention=raw_input("Destination extention [JPG,jpeg,png,bmp]: ")
 if destination_extention == "":
     destination_extention = "jpg"
-print "Found %d %s images in %s\n"%(len(imagelist),filelist[0].split(".")[-1],path)
 
-w = int(raw_input("Height: "))
-h = int(raw_input("Width: "))
+# Select output geometries
+w = int(raw_input("Output height: "))
+h = int(raw_input("Output width: "))
 
+# Populate filename list
 for f in filelist:
     imagelist.append(f.replace(path,""))
-                
+
+# Start conversion
 for file in imagelist:
     c+=1
     with open(path+"/"+file, 'r+b') as f:
