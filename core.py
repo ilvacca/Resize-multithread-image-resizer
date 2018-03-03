fdbd43f313209e2bd21432519d100527cbc4c09c
Alessio Vaccaro <alexvac@hotmail.it>
2018-03-03 20:59:44 +0100

    menu

--- core.py
+++ core.py
@@ -5,28 +5,14 @@ import datetime
 import Tkinter
 import tkFileDialog
 
+from menu import menu
+
 selected_font = "Helvetica"
 
 def actual_time():
     return datetime.datetime.now().strftime("%H:%M:%S")
 
-class menu:
 
-    def __init__(self,root):
-        self.menubar = Tkinter.Menu(root)
-
-        self.filemenu = Tkinter.Menu(self.menubar, tearoff=0)
-        self.filemenu.add_command(label="Open")
-        self.filemenu.add_separator()
-        self.filemenu.add_command(label="Exit")
-        self.menubar.add_cascade(label="File", menu=self.filemenu)
-
-        self.helpmenu = Tkinter.Menu(self.menubar, tearoff=0)
-        self.helpmenu.add_command(label="Help")
-        self.helpmenu.add_command(label="About")
-        self.menubar.add_cascade(label="?", menu=self.helpmenu)
-
-        root.config(menu=self.menubar)
 
 class header:
     
